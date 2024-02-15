2616457b27d533c17722c2a88b13e4dcf9c4aaba
Daniel <derachukwu9@gmail.com>
2024-02-15 12:34:21 +0100

    removed all the taliwind files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router,Link, Route, Routes} from 'react-router-dom'
 
 function Logo(){
   return(
-    <span className='font-black text-secondary-color '>DB</span>
+    <span> DB</span>
   )
 
   // border-2 border-rose-500
@@ -21,40 +21,25 @@ function Nav(){
   let [open,setOpen] = useState(false);
   
   return(
-  <nav className='fixed top-0 w-full  bg-nav-bar-bg-color-sm md:bg-primary-color '>
-{/* border-2 border-green-500 */}
-    <div className='md:flex items-center justify-between p-5 pr-10 '>
+  <nav >
+
+    <div >
     {/* border-2 border-yellow-300 */}
 
       <Logo />
 
-      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-5 top-5 cursor-pointer
-      md:hidden 
-      '>
+      <div >
 
         <ion-icon name={open?'close':'menu'}></ion-icon>
 
       </div>
 
-      <ul className={ `flex flex-col md:flex-row md:item-center md:justify-between     
-       md:static absolute left-0 
-       md:pl-0 pl-5
-       md:pb-0 pb-4
-       mt:pb-0 pt-2
-       bg-nav-bar-bg-color-sm md:bg-primary-color
-       md:z-auto z-[-1] w-full md:w-auto 
-       transition-all duration-100 ease-in
-       
-      ${open ? 'top-auto opacity-100':'top-[-490px] opacity-0'} 
-      md:opacity-100`}>
+      <ul>
         {/* border-2 border-rose-500 */}
 
         {
           links.map(link=>(
-            <Link key={link.name} to={link.link} className='md:mr-8
-            my-2 md:my-0 
-            
-            text-secondary-color font-main font-extrabold text-xs'> {link.name} </Link>
+            <Link key={link.name} to={link.link} > {link.name} </Link>
             ))
         }
 
@@ -69,12 +54,12 @@ function Home(){
 
   return(
     <>
-      <div class='page'>
+      <div >
 
           <h1 
-          class="main-text">HELLO <br /> DONUTS</h1> 
+          >HELLO <br /> DONUTS</h1> 
 
-          <p class='sub-text '>A healthy taste of deliciousness</p>
+          <p>A healthy taste of deliciousness</p>
 
       </div>
 
@@ -87,8 +72,8 @@ function Home(){
 
 function About(){
   return(
-    <div class="page">
-      <h1 class="main-text">About us</h1>
+    <div >
+      <h1 >About us</h1>
       <p>Leran more about our company and mission</p>
     </div>
   )
@@ -97,8 +82,8 @@ function About(){
 
 function Product(){
   return(
-    <div class="page">
-      <h1 class="main-text"> Products</h1>
+    <div >
+      <h1 > Products</h1>
       <p>This is a line up of our products</p>
     </div>
   )
@@ -107,8 +92,8 @@ function Product(){
 
 function Contact(){
   return(
-    <div class="page">
-      <h1 class="main-text">Contact us</h1>
+    <div>
+      <h1>Contact us</h1>
       <p>Get in touch with us!</p>
     </div>
   )
@@ -121,7 +106,7 @@ function App() {
         <Nav />
         
         
-        <div className=' bg-primary-color  h-screen ' > 
+        <div > 
           <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='about' element={<About/>} />
